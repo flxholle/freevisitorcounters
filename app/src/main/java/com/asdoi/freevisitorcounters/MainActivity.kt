e9eefd069ddd5652d33c4594add9e3e2906f2e6a
asdoi <asdoi@t-online.de>
2020-07-09 21:58:11 +0200

    used another charts library

--- app/src/main/java/com/asdoi/freevisitorcounters/MainActivity.kt
+++ app/src/main/java/com/asdoi/freevisitorcounters/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
 import android.preference.PreferenceManager
@@ -18,18 +19,16 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
 import com.google.android.material.textfield.TextInputEditText
+import im.dacer.androidcharts.BarView
+import im.dacer.androidcharts.LineView
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var visitorCounter: VisitorCounter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,6 +74,146 @@ class MainActivity : AppCompatActivity() {
         setDailyAlarm()
     }
 
+    private fun loadSite(url: String) {
+        Thread {
+            visitorCounter = Parser.parseWebsite(url)
+            runOnUiThread {
+                generateUI(visitorCounter)
+            }
+        }.start()
+    }
+
+    private fun generateUI(visitorCounter: VisitorCounter?) {
+        findViewById<LinearLayout>(R.id.main_linear).removeAllViews()
+
+        if (visitorCounter != null) {
+            //Domain name
+            findViewById<TextView>(R.id.domain_tV).text = visitorCounter.domain
+
+            //Overview
+            val overviewView = layoutInflater.inflate(R.layout.overview, null)
+            val overview = visitorCounter.overview
+            overviewView.findViewById<TextView>(R.id.title).text = overview.title
+            overviewView.findViewById<TextView>(R.id.title).paintFlags =
+                Paint.UNDERLINE_TEXT_FLAG
+            overviewView.findViewById<TextView>(R.id.textView1).text = overview.sinceTitle
+            overviewView.findViewById<TextView>(R.id.textView2).text =
+                overview.prognosisTitle
+            overviewView.findViewById<TextView>(R.id.textView3).text = overview.bestDayTitle
+            overviewView.findViewById<TextView>(R.id.textView4).text =
+                overview.bestDayVisitorsTitle
+
+            var df: DateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+
+            overviewView.findViewById<TextView>(R.id.textView11).text =
+                df.format(overview.since)
+            overviewView.findViewById<TextView>(R.id.textView21).text = overview.prognosis
+            overviewView.findViewById<TextView>(R.id.textView31).text =
+                df.format(overview.bestDay)
+            overviewView.findViewById<TextView>(R.id.textView41).text =
+                overview.bestDayVisitors.toString()
+
+            findViewById<LinearLayout>(R.id.main_linear).addView(overviewView)
+
+            //Visitors Overview
+            val visitorsOverviewView = layoutInflater.inflate(R.layout.overview, null)
+            val visitorsOverview = visitorCounter.visitorsOverview
+            visitorsOverviewView.findViewById<TextView>(R.id.title).text =
+                visitorsOverview.title
+            visitorsOverviewView.findViewById<TextView>(R.id.title).paintFlags =
+                Paint.UNDERLINE_TEXT_FLAG
+            visitorsOverviewView.findViewById<TextView>(R.id.textView1).text =
+                visitorsOverview.todayTitle
+            visitorsOverviewView.findViewById<TextView>(R.id.textView2).text =
+                visitorsOverview.yesterdayTitle
+            visitorsOverviewView.findViewById<TextView>(R.id.textView3).text =
+                visitorsOverview.allTitle
+            visitorsOverviewView.findViewById<TextView>(R.id.textView4).text =
+                visitorsOverview.onlineTitle
+
+            visitorsOverviewView.findViewById<TextView>(R.id.textView11).text =
+                visitorsOverview.today.toString()
+            visitorsOverviewView.findViewById<TextView>(R.id.textView21).text =
+                visitorsOverview.yesterday.toString()
+            visitorsOverviewView.findViewById<TextView>(R.id.textView31).text =
+                visitorsOverview.all.toString()
+            visitorsOverviewView.findViewById<TextView>(R.id.textView31)
+                .setTextColor(ContextCompat.getColor(this, R.color.colorAccent))
+            visitorsOverviewView.findViewById<TextView>(R.id.textView41).text =
+                visitorsOverview.online.toString()
+
+            findViewById<LinearLayout>(R.id.main_linear).addView(visitorsOverviewView)
+
+
+            val chartView = layoutInflater.inflate(R.layout.line_chart, null)
+            chartView.findViewById<TextView>(R.id.title).text =
+                visitorCounter.last30DaysTitle
+            chartView.findViewById<TextView>(R.id.title).paintFlags =
+                Paint.UNDERLINE_TEXT_FLAG
+
+            val line = arrayListOf<Int>()
+            for (i in visitorCounter.last30Days)
+                line.add(0, i)
+            val bottom = arrayListOf<String>();
+            for (i in 1..line.size)
+                bottom.add(i.toString())
+
+            val lineView: LineView = chartView.findViewById(R.id.line_view)
+            lineView.setDrawDotLine(true) //optional
+            lineView.setShowPopup(LineView.SHOW_POPUPS_All) //optional
+            lineView.setBottomTextList(bottom)
+            lineView.setColorArray(
+                intArrayOf(
+                    ContextCompat.getColor(
+                        this, R.color.colorAccent
+                    )
+                )
+            )
+            lineView.setDataList(arrayListOf(line))
+
+            val barView = chartView.findViewById<BarView>(R.id.bar_view)
+            barView.setBottomTextList(bottom)
+            barView.setDataList(line, line.max() as Int + 1)
+            barView.setBarColorEmptyPart(Color.TRANSPARENT)
+            barView.setBarColorValuePart(ContextCompat.getColor(this, R.color.colorAccent))
+
+            if (isLineChart()) {
+                lineView.visibility = View.VISIBLE
+                barView.visibility = View.GONE
+            } else {
+                lineView.visibility = View.GONE
+                barView.visibility = View.VISIBLE
+            }
+
+            findViewById<LinearLayout>(R.id.main_linear).addView(chartView)
+
+
+            val last10DaysView = layoutInflater.inflate(R.layout.last_10_days, null)
+            last10DaysView.findViewById<TextView>(R.id.title).text =
+                visitorCounter.last10VisitorsTitle
+            last10DaysView.findViewById<TextView>(R.id.title).paintFlags =
+                Paint.UNDERLINE_TEXT_FLAG
+            df = SimpleDateFormat("dd.MM.yyyy - hh:mm", Locale.getDefault())
+            for (i in visitorCounter.last10Visitors) {
+                val date = layoutInflater.inflate(R.layout.date, null) as TextView
+                date.text = df.format(i)
+                last10DaysView.findViewById<LinearLayout>(R.id.last_10_days).addView(date)
+            }
+
+            findViewById<LinearLayout>(R.id.main_linear).addView(last10DaysView)
+
+            val credits = TextView(this)
+            credits.setText(R.string.chart_library_credit)
+            credits.setPadding(8, 30, 8, 0)
+            findViewById<LinearLayout>(R.id.main_linear).addView(credits)
+
+
+        } else {
+            findViewById<TextView>(R.id.domain_tV).text =
+                getString(R.string.no_internet_connection)
+        }
+    }
+
     private fun setDailyAlarm() {
         val calendar = Calendar.getInstance()
         calendar[Calendar.HOUR_OF_DAY] = 12
@@ -99,114 +238,6 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun loadSite(url: String) {
-        Thread {
-            val visitorCounter = Parser.parseWebsite(url)
-            runOnUiThread {
-                findViewById<LinearLayout>(R.id.main_linear).removeAllViews()
-
-                if (visitorCounter != null) {
-                    //Domain name
-                    findViewById<TextView>(R.id.domain_tV).text = visitorCounter.domain
-
-                    //Overview
-                    val overviewView = layoutInflater.inflate(R.layout.overview, null)
-                    val overview = visitorCounter.overview
-                    overviewView.findViewById<TextView>(R.id.title).text = overview.title
-                    overviewView.findViewById<TextView>(R.id.title).paintFlags =
-                        Paint.UNDERLINE_TEXT_FLAG
-                    overviewView.findViewById<TextView>(R.id.textView1).text = overview.sinceTitle
-                    overviewView.findViewById<TextView>(R.id.textView2).text =
-                        overview.prognosisTitle
-                    overviewView.findViewById<TextView>(R.id.textView3).text = overview.bestDayTitle
-                    overviewView.findViewById<TextView>(R.id.textView4).text =
-                        overview.bestDayVisitorsTitle
-
-                    var df: DateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
-
-                    overviewView.findViewById<TextView>(R.id.textView11).text =
-                        df.format(overview.since)
-                    overviewView.findViewById<TextView>(R.id.textView21).text = overview.prognosis
-                    overviewView.findViewById<TextView>(R.id.textView31).text =
-                        df.format(overview.bestDay)
-                    overviewView.findViewById<TextView>(R.id.textView41).text =
-                        overview.bestDayVisitors.toString()
-
-                    findViewById<LinearLayout>(R.id.main_linear).addView(overviewView)
-
-                    //Visitors Overview
-                    val visitorsOverviewView = layoutInflater.inflate(R.layout.overview, null)
-                    val visitorsOverview = visitorCounter.visitorsOverview
-                    visitorsOverviewView.findViewById<TextView>(R.id.title).text =
-                        visitorsOverview.title
-                    visitorsOverviewView.findViewById<TextView>(R.id.title).paintFlags =
-                        Paint.UNDERLINE_TEXT_FLAG
-                    visitorsOverviewView.findViewById<TextView>(R.id.textView1).text =
-                        visitorsOverview.todayTitle
-                    visitorsOverviewView.findViewById<TextView>(R.id.textView2).text =
-                        visitorsOverview.yesterdayTitle
-                    visitorsOverviewView.findViewById<TextView>(R.id.textView3).text =
-                        visitorsOverview.allTitle
-                    visitorsOverviewView.findViewById<TextView>(R.id.textView4).text =
-                        visitorsOverview.onlineTitle
-
-                    visitorsOverviewView.findViewById<TextView>(R.id.textView11).text =
-                        visitorsOverview.today.toString()
-                    visitorsOverviewView.findViewById<TextView>(R.id.textView21).text =
-                        visitorsOverview.yesterday.toString()
-                    visitorsOverviewView.findViewById<TextView>(R.id.textView31).text =
-                        visitorsOverview.all.toString()
-                    visitorsOverviewView.findViewById<TextView>(R.id.textView41).text =
-                        visitorsOverview.online.toString()
-
-                    findViewById<LinearLayout>(R.id.main_linear).addView(visitorsOverviewView)
-
-
-                    val chartView = layoutInflater.inflate(R.layout.line_chart, null)
-                    chartView.findViewById<TextView>(R.id.title).text =
-                        visitorCounter.last30DaysTitle
-                    chartView.findViewById<TextView>(R.id.title).paintFlags =
-                        Paint.UNDERLINE_TEXT_FLAG
-                    val chart = chartView.findViewById<LineChart>(R.id.chart1)
-                    chart.isDragEnabled = true
-                    chart.setScaleEnabled(true)
-                    chart.isDoubleTapToZoomEnabled = true
-                    chart.setPinchZoom(false)
-                    chart.setNoDataText(getString(R.string.no_visitors_for_the_last_30_days))
-                    chart.description.isEnabled = false
-
-                    chart.data = getData(visitorCounter.last30Days)
-                    chart.legend.isEnabled = false
-                    chart.axisLeft.isEnabled = true
-                    chart.axisRight.isEnabled = false
-                    chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-                    chart.animateX(1000)
-
-                    findViewById<LinearLayout>(R.id.main_linear).addView(chartView)
-
-
-                    val last10DaysView = layoutInflater.inflate(R.layout.last_10_days, null)
-                    last10DaysView.findViewById<TextView>(R.id.title).text =
-                        visitorCounter.last10VisitorsTitle
-                    last10DaysView.findViewById<TextView>(R.id.title).paintFlags =
-                        Paint.UNDERLINE_TEXT_FLAG
-                    df = SimpleDateFormat("dd.MM.yyyy - hh:mm", Locale.getDefault())
-                    for (i in visitorCounter.last10Visitors) {
-                        val date = layoutInflater.inflate(R.layout.date, null) as TextView
-                        date.text = df.format(i)
-                        last10DaysView.findViewById<LinearLayout>(R.id.last_10_days).addView(date)
-                    }
-
-                    findViewById<LinearLayout>(R.id.main_linear).addView(last10DaysView)
-
-                } else {
-                    findViewById<TextView>(R.id.domain_tV).text =
-                        getString(R.string.no_internet_connection)
-                }
-            }
-        }.start()
-    }
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -226,20 +257,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getData(visitors: List<Int>): LineData {
-        val values: ArrayList<Entry> = ArrayList()
-        for (i in visitors.indices) {
-            values.add(Entry(i.toFloat(), visitors[i].toFloat()))
-        }
-
-        val dataSet = LineDataSet(values, "DataSet 1")
-        dataSet.lineWidth = 3f
-        dataSet.circleRadius = 4f
-        dataSet.color = ContextCompat.getColor(this, R.color.colorPrimary)
-        dataSet.setCircleColor(ContextCompat.getColor(this, R.color.colorAccent))
-
-        // create a data object with the data sets
-        return LineData(dataSet)
+    override fun onClick(p0: View?) {
+        setLineChart(!isLineChart())
+        generateUI(visitorCounter)
     }
 
     private fun getLastURL(): String {
@@ -249,6 +269,15 @@ class MainActivity : AppCompatActivity() {
     private fun setLastURL(value: String) {
         PreferenceManager.getDefaultSharedPreferences(this).edit().putString("last_url", value)
             .apply()
+    }
+
+    private fun isLineChart(): Boolean {
+        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean("line_chart", true)
+    }
+
+    private fun setLineChart(value: Boolean) {
+        PreferenceManager.getDefaultSharedPreferences(this).edit().putBoolean("line_chart", value)
+            .commit()
     }
 
     fun hideKeyboard(activity: Activity) {
